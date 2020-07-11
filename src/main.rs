c0f18535ad37194aa82719594e1ffd3ba51e833f
evilmurries <cpiwarski00@gmail.com>
2020-07-10 22:03:05 -0700

    args: Parse args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file = &args[2];
+    
+    println!("args: {:?}", args);
+    println!("query: {}, file: {}", query, file);
 }
