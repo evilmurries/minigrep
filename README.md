bc897a4f9cc1d6d565a4c8254d7824de748ccc6b
evilmurries <cpiwarski00@gmail.com>
2020-07-11 18:39:22 -0700

    config: Move arms to struct

---
+++ README.md
@@ -0,0 +1,10 @@
+# Minigrep
+
+## What is it?
+
+An implementation of grep written in Rust.
+
+## Why??
+
+Mostly so I can learn / practice Rust. This project is from chapter
+12 of *The Rust Programming Language*.
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
 use std::env;
+use std::fs;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file = &args[2];
+    let config = Config::new(&args);    
     
-    println!("args: {:?}", args);
-    println!("query: {}, file: {}", query, file);
+    println!("Searching for: {}", config.query);
+    println!("Searching in: {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Failure reading file");
+    println!("Contents: {}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+    
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config {query, filename}
+    }
 }
