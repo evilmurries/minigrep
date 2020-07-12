deac68fa3e278f1d129394d1408a84ba18d7175b
evilmurries <cpiwarski00@gmail.com>
2020-07-11 21:54:19 -0700

    search: Created test

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ An implementation of grep written in Rust.
 
 Mostly so I can learn / practice Rust. This project is from chapter
 12 of *The Rust Programming Language*.
+
+If you have any suggestions or comments that might help me to learn, or
+as best practices for a new college grad, please feel free to create an issue
+with your advice! I would greatly appreciate it.
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,16 @@
 use std::env;
-use std::fs;
+use std::process;
 
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);    
+    let config = Config::new(&args).unwrap_or_else( | err | {
+        process::exit(1);
+    });    
     
-    println!("Searching for: {}", config.query);
-    println!("Searching in: {}", config.filename);
-
-    let contents = fs::read_to_string(config.filename)
-        .expect("Failure reading file");
-    println!("Contents: {}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Config {query, filename}
-    }
+    if let Err(e) = minigrep::run(config) {
+        process::exit(1);
+    };
 }
